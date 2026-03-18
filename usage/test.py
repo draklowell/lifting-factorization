1bf36261743fe7c37f1f3bc5bf75c5828eaa197f
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2026-03-18 14:09:59 +0200

    feat: add basic usage example

---
+++ usage/test.py
@@ -0,0 +1,58 @@
+import json
+import os
+import sys
+
+import numpy as np
+from pywt import Wavelet, dwt
+
+from lifting import LiftingScheme
+
+sys.set_int_max_str_digits(1000000)
+
+
+def print_cmp(a, b, name):
+    err = np.abs(a - b)
+    err_mse = np.sum(err**2) / len(err)
+    message = " " + name.upper() + ": "
+    message = message + " " * (17 - len(message))
+
+    if err_mse < 1e-14:
+        message += f"\033[32m{err_mse:.2e}\033[0m"
+    elif err_mse < 1e-7:
+        message += f"\033[33m{err_mse:.2e}\033[0m"
+    else:
+        message += f"\033[31m{err_mse:.2e}\033[0m"
+
+    print(message)
+
+
+counter = 0
+for filename in os.listdir("coeffs-new/"):
+    if not filename.endswith(".json"):
+        continue
+
+    wavelet = filename.removesuffix(".json")
+    print(f"WAVELET: {wavelet}")
+
+    with open(f"coeffs/{filename}") as f:
+        data = json.load(f)
+
+    wavelet = Wavelet(wavelet)
+
+    scheme = LiftingScheme.from_object(data, "symmetric")
+
+    test_signal = np.random.normal(scale=1, size=(1001,))
+
+    approx_lifting, details_lifting = scheme.forward(test_signal)
+    approx_dwt, details_dwt = dwt(test_signal, wavelet=wavelet, mode="symmetric")
+
+    print_cmp(approx_dwt, approx_lifting, "approximation")
+    print_cmp(details_dwt, details_lifting, "details")
+
+    data_lifting = scheme.inverse(approx_lifting.data, details_lifting.data)
+    data_lifting = data_lifting[: len(test_signal)]
+
+    print_cmp(test_signal, data_lifting, "reconstruction")
+    counter += 1
+
+print(f"Tested {counter} wavelets")
